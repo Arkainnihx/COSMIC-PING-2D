240e1e9c3c00920d5d09236dc460ffbce9b2d9c0
William J. C. Shepherd <18550@totton.ac.uk>
2022-02-28 16:27:06 +0000

    Iterate on paddle and orb prefabs, spawn paddles and orb on start game

--- COSMIC PING 2D/Assets/Scripts/PaddleController.cs
+++ COSMIC PING 2D/Assets/Scripts/PaddleController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
+    public int playerID;
+
     // Start is called before the first frame update
     void Start()
     {
--- COSMIC PING 2D/Assets/Scripts/PlayingFieldController.cs
+++ COSMIC PING 2D/Assets/Scripts/PlayingFieldController.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class PlayingFieldController : MonoBehaviour
 {
+    public GameObject paddlePrefab;
+    public GameObject orbPrefab;
+    public int totalPlayers = 2;
+
+    private GameObject[] paddleArray;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (totalPlayers == 2)
+        {
+            paddleArray = new GameObject[2];
+            paddleArray[0] = Instantiate(paddlePrefab, new Vector3(15, 0), Quaternion.Euler(0, -90f, 0));
+            paddleArray[0].GetComponent<PaddleController>().playerID = 1;
+            paddleArray[1] = Instantiate(paddlePrefab, new Vector3(-15, 0), Quaternion.Euler(180, -90f, 0));
+            paddleArray[0].GetComponent<PaddleController>().playerID = 2;
+        }
+        Instantiate(orbPrefab, Vector3.zero, Quaternion.identity);
     }
 
     // Update is called once per frame
